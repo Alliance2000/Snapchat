ec8cf936778f3cc36d13da342aaa6bdb444f224d
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-15 00:11:48 -0700

    ImageViewController implemented

---
+++ Snapchat/PictureViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PictureViewController.swift
+//  Snapchat
+//
+//  Created by Marcus Jessnitz on 15.01.18.
+//  Copyright © 2018 Marcus Jessnitz. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func nextTapped(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+}
