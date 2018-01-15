39248a6c5751f6211b09793a346a9a3c472f18ee
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-14 22:45:31 -0700

    Firebase initialized

--- Snapchat/AppDelegate.swift
+++ Snapchat/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+// Firebase Import
+import Firebase
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Firebase Import
+        FirebaseApp.configure()
+        
         return true
     }
 
