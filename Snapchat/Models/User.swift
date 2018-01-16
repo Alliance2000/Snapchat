8adb23b02157d8d6e1b94ef93ea4961c09c01442
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-15 21:49:16 -0700

    Users show up

---
+++ Snapchat/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Snapchat
+//
+//  Created by Marcus Jessnitz on 15.01.18.
+//  Copyright © 2018 Marcus Jessnitz. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
--- Snapchat/ViewControllers/SelectUserViewController.swift
+++ Snapchat/ViewControllers/SelectUserViewController.swift
@@ -7,30 +7,45 @@
 //
 
 import UIKit
+import FirebaseDatabase
+
 
 class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
     @IBOutlet weak var tableView: UITableView!
     
+    var users : [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        Database.database().reference().child("users").observe(DataEventType.childAdded, with: {(snapshot) in
+            
+            let user = User()
+            user.email = snapshot.childSnapshot(forPath: "email").value as! String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            self.tableView.reloadData()
+        })
     }
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return users.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let user = users[indexPath.row]
         
-        return UITableViewCell()
+        cell.textLabel?.text = user.email
+        return cell
     }
 
 }
